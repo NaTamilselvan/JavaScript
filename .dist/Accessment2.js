0205b35b01ed521ce234485a2c1de790123214d1
N.Tamilselvan <natamilselvanme@gmail.com>
2025-04-15 17:32:35 +0530

    These are today practished qestions

---
+++ .dist/Accessment2.js
@@ -0,0 +1,9 @@
+var FruitName="banana" , count=5 ,price=15;
+
+var total=price*count;
+
+console.log(FruitName+"total price "+total);
+
+const t=(a,b)=>{a*b};
+
+console.log(t);
---
+++ .dist/functiontrial.js
@@ -0,0 +1,20 @@
+
+/*  This program i learned function , parameters and NaN(Not a Number);
+function is block of code that performs particular task when it called.
+once you create a function we cannot be rewrite only use it. */
+
+/* paremeter is term its variable or placeholder to create in method definition 
+parameters acts as a input of a function its allow to pass data to the function when it called*/ 
+
+
+//create function syntax is function myfunctionname(){}
+
+function area(length,breadth){
+
+    console.log("length is :"+length,"breadth is :"+breadth);
+
+    console.log(length*breadth);
+}
+
+
+area(5,10);
